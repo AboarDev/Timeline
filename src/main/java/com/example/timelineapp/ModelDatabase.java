c0a20e2289f1d7a4f3a28f52e6a10557d8885072
Sam <sksmiller@gmail.com>
2020-11-19 12:52:35 +1300

    "final-ish version"

--- src/main/java/com/example/timelineapp/ModelDatabase.java
+++ src/main/java/com/example/timelineapp/ModelDatabase.java
@@ -12,6 +12,7 @@ import androidx.room.RoomDatabase;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -39,12 +40,11 @@ public abstract class ModelDatabase extends RoomDatabase {
 
     public static Uri getURI(Context context, int id){
         Resources resources = context.getResources();
-        Uri uri = new Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        return new Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
                 .authority(resources.getResourcePackageName(id))
                 .appendPath(resources.getResourceTypeName(id))
                 .appendPath(resources.getResourceEntryName(id))
                 .build();
-        return uri;
     }
 
     private static volatile ModelDatabase INSTANCE;
@@ -60,34 +60,34 @@ public abstract class ModelDatabase extends RoomDatabase {
                             .addMigrations(MIGRATION_2_3).addCallback(new RoomDatabase.Callback() {
                         public void onCreate (@NonNull SupportSQLiteDatabase db) {
                             super.onOpen(db);
-                            Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
-                                @Override
-                                public void run() {
-                                    //Improved examples for initial prototype
-                                    ModelDatabase modelDatabase = getInstance(context);
-                                    Timeline timeline = new Timeline("Example",
-                                            "A simple example timeline without times shown",
-                                            false);
-                                    modelDatabase.timelineDao().insert(timeline);
-                                    int id = modelDatabase.timelineDao().getByName("Example")
-                                            .timelineID;
-                                    Entry entry = new Entry(id,"An example entry",
-                                            "This entry has no images",1,"");
-                                    entry.URI = getURI(context,R.raw.old_image).toString();
-                                    modelDatabase.entryDao().insert(entry);
-                                    entry = new Entry(id,"Another entry","This Entry has an image",2,
-                                            "");
-                                    /*Resources resources = context.getResources();
-                                    Uri uri = new Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-                                            .authority(resources.getResourcePackageName(R.raw.old_image))
-                                            .appendPath(resources.getResourceTypeName(R.raw.old_image))
-                                            .appendPath(resources.getResourceEntryName(R.raw.old_image))
-                                            .build();*/
-                                    entry.URI = getURI(context,R.raw.m20200816_153134).toString();
-                                    modelDatabase.entryDao().insert(entry);
-                                    timeline = new Timeline("Example #2","A simple example timeline with times shown",true);
-                                    modelDatabase.timelineDao().insert(timeline);
-                                }
+                            Executors.newSingleThreadScheduledExecutor().execute(() -> {
+                                //Improved examples for initial prototype
+                                ModelDatabase modelDatabase = getInstance(context);
+                                SimpleDateFormat formatTime = new SimpleDateFormat("dd/MM/yyyy - h:mm");
+                                Timeline timeline = new Timeline("Example",
+                                        "A simple example timeline without times shown",
+                                        false);
+                                modelDatabase.timelineDao().insert(timeline);
+                                int id = modelDatabase.timelineDao().getByName("Example")
+                                        .timelineID;
+                                Entry entry = new Entry(id,"An example entry",
+                                        "This entry has no images",1,"");
+                                modelDatabase.entryDao().insert(entry);
+                                entry = new Entry(id,"Another entry","This Entry has an image",2,
+                                        "");
+                                entry.URI = getURI(context,R.raw.m20200816_153134).toString();
+                                modelDatabase.entryDao().insert(entry);
+                                timeline = new Timeline("Example #2","A simple example timeline with times shown",true);
+                                modelDatabase.timelineDao().insert(timeline);
+                                id = modelDatabase.timelineDao().getByName("Example #2")
+                                        .timelineID;
+                                String time = "23/09/2020";
+                                entry = new Entry(id,"An entry with a date","",1,time);
+                                modelDatabase.entryDao().insert(entry);
+                                time = "23/09/2020 - 4:38pm";
+                                entry = new Entry(id,"An entry with a time","This entry has an image",2,time);
+                                entry.URI = getURI(context,R.raw.m20200923_163831).toString();
+                                modelDatabase.entryDao().insert(entry);
                             });
                         }
                         public void onOpen (@NonNull SupportSQLiteDatabase db) {
